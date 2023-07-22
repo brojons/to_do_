1cb1fa6e2911c72baed21757aae5d12f76619152
Sardorbekmaxmud <sardorbekmaxmudov33@gmail.com>
2023-07-22 17:43:32 +0500

    added new files and translate in i18n db

--- config/settings.py
+++ config/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
     'to_do',
 
     #third
+    'config',
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
+    'modeltranslation',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,6 +58,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'to_do.CustomerUser'
 
 MIDDLEWARE = [
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -69,6 +73,30 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    # Add your frontend's domain or "*" to allow requests from any origin
+    'http://localhost:8000',
+    'http://localhost:5431',
+]
+
+# CSRF_TRUSTED_ORIGINS = [
+    # "http://localhost:8000",
+# ]
+
+# Optional: If you want to allow specific HTTP methods or headers
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+CORS_ALLOW_HEADERS = [
+    'Content-Type',
+    'Authorization',
+]
 
 ROOT_URLCONF = 'config.urls'
 
@@ -128,7 +156,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uz'
+
+LANGUAGES = (
+    ('uz','uzbek'),
+    ('en','english'),
+    ('ru','russian'),
+)
 
 TIME_ZONE = 'UTC'
 
@@ -136,6 +170,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MODELTRANSLATION_LANGUAGES = ('uz','en','ru')
+MODELTRANSLATION_TRANSLATION_REGISTRY = 'config.translation'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
